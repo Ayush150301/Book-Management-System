c9a3ca5d96b9124d11778d53405342050100d2d4
[firstname <[ayushpandey150301@gmail.com]>
2023-12-17 17:48:47 +0530

    solving the connection esatblishing error

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"fmt"
+	// "fmt"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -13,16 +13,16 @@ var (
 
 func Connect() {
 
-	// Replace placeholders with your actual MySQL details
-	username := "ayush1503"
-	password := "ayush1503@12"
-	host := "127.0.0.1"
-	port := "3306"
-	dbName := "simplerest"
+	// username := "ayush1503"
+	// password := "ayush1503@12"
+	// host := "127.0.0.1"
+	// port := "3306"
+	// dbName := "simplerest"
 
-	// Create a connection string with proper encoding
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		username, password, host, port, dbName)
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+	// 	username, password, host, port, dbName)
+
+	dsn := "ayush:ayush1503@tcp(hostname:port)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 
 	d, err := gorm.Open("mysql", dsn)
 	if err != nil {
